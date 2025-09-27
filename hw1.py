d1455cbf7e4c555298e23345bad5cb764f76d34f
23021473vuvietanh <23021473@vnu.edu.vn>
2025-09-27 20:49:55 +0700

    inp

--- hw1.py
+++ hw1.py
@@ -2,15 +2,15 @@ def main():
     t, ph, h = map(float, input().split())
 
     if t < 20 or t > 30:
-        print("Lỗi đầu vào, vui lòng thử lại!")
+        print("Lỗi input")
         return -1
 
     if ph < 4 or ph > 7:
-        print("Lỗi đầu vào, vui lòng thử lại!")
+        print("Lỗi input")
         return -1
 
     if h < 0 or h > 5.5:
-        print("Lỗi đầu vào, vui lòng thử lại!")
+        print("Lỗi input")
         return -1
 
     if t >= 27 and ph >= 6.5 and h >= 4.5:
