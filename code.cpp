9ac0c7f4756e418c4466c5bf998ef2338a72486b
23021473vuvietanh <23021473@vnu.edu.vn>
2025-09-27 19:10:36 +0700

    version 1

--- code.cpp
+++ code.cpp
@@ -3,41 +3,41 @@ using namespace std;
 
 int main()
 {
-        double t;
-        double ph;
-        double h;
+    double t;
+    double ph;
+    double h;
 
-        cin >> t >> ph >> h;
+    cin >> t >> ph >> h;
 
-        if (t < 10 or t > 30) {
-            cout << "Loi input" << endl;
-            return -1;
+    if (t < 10 or t > 30) {
+        cout << "Loi input" << endl;
+        return -1;
 
-        }
-        if (ph < 4 or ph > 7) {
-            cout << "Loi input" << endl;
-            return -1;
-        }
+    }
+    if (ph < 4 or ph > 7) {
+        cout << "Loi input" << endl;
+        return -1;
+    }
 
-        if (h < 0 or h > 5.5) {
-            cout << "Loi input" << endl;
-            return -1;
-        }
+    if (h < 0 or h > 5.5) {
+        cout << "Loi input" << endl;
+        return -1;
+    }
 
-        if (t >= 27 and ph >= 6.5 and h >= 4.5) {
-            cout << "Tha bachtuoc" << endl;
-        }
+    if (t >= 27 and ph >= 6.5 and h >= 4.5) {
+        cout << "Tha bachtuoc" << endl;
+    }
 
-        else if (t >= 25 and ph >= 5.5 and h >= 4.5) {
-            cout << "Tha ca" << endl;
-        }
+    else if (t >= 25 and ph >= 5.5 and h >= 4.5) {
+        cout << "Tha ca" << endl;
+    }
 
-        else if (t >= 25 and ph >= 4 and h >= 3) {
-            cout << "Tha tom" << endl;
-        }
+    else if (t >= 25 and ph >= 4 and h >= 3) {
+        cout << "Tha tom" << endl;
+    }
 
-        else {
-            cout << "nothing!" << endl;
-        }
+    else {
+        cout << "nothing!" << endl;
+    }
     return 0;
 }
